70b0a10bd5c12afe999890ec2e7adf32b7d4d2ed
Vinicius Mesti <vinimesti88@gmail.com>
2024-09-11 15:58:40 -0300

    NWL pocket

---
+++ src/http/server.ts
@@ -0,0 +1,40 @@
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoal } from '../functions/create-goal'
+import z from 'zod'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
